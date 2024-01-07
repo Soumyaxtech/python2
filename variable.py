ed24887d5426f301d793e9a27fb5296da67ee9ec
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-01-07 09:56:47 +0530

    variables in python and there typecasting

--- variable.py
+++ variable.py
@@ -1,7 +1,34 @@
-firstname = "Soumyajit"
-lastname = "Koley"
-nickname = "babai"
-age = 22
-print(firstname + " " + lastname)
-print(nickname)
-print("age is " + str(age) )
+# firstname = "Soumyajit"
+# lastname = "Koley"
+# nickname = "babai"
+# age = 22
+# print(firstname + " " + lastname)
+# print(nickname)
+# print("age is " + str(age) )
+
+# how to take user input of variable
+
+# name = input("enter your name ")
+# print("hello " + name)
+
+# print("hello Tony ")
+# var1 = input("what is your superhero name ?")
+# print(var1)
+
+# TYPE CASTING
+
+# old_age = input("enter age ")
+# new_age = int(old_age) + 10    # here old_age is converted into int by type casting
+# print("new age is " + str(new_age))
+# num = input("enter a number")
+# print(float(num))
+# print(str(num))
+
+# program of sum of two number
+
+num1 = input("enter 1st number ")
+num2 = input("enter 2nd number ")
+sum1 = int(num1) + int(num2)
+sum2 = num1+num2
+print(sum1) # take as int for typecasting so ans 23+67=90
+print(sum2) # take value as string so ans 2367
