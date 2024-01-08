59965b316db33f517d4981c1fd212ab9a0f63235
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-01-08 09:52:15 +0530

    string operation

--- string.py
+++ string.py
@@ -1,7 +1,12 @@
 name = input("enter a string ")
 # DIFF STRING OPERATIONS**************
-print(name.upper())
-print(name.lower())
+print(name.upper())  # uppercase
+print(name.lower())  # lowercase
 val = input("what letter you want to find ")
 print(name.find(val))  # it will give you the index of the letter if present
-
+name1 = input("enter string to replace ")
+print(name.replace(name,name1))  # it will replace the string
+val1= input("what letter you want to find ")
+print("val1" in name)  # it will return true or false
+print(name)
+print(name.__len__())
