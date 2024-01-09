812c9eb4b2b8360744a6f557ca8f8f0d2ca6d6a2
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-01-09 10:07:07 +0530

    comparision operations

---
+++ compar-operator.py
@@ -0,0 +1,7 @@
+# it compare two value and return true or false
+print(3 > 2)    # greater than
+print(3 < 2)    # less than
+print(3 >= 2)   # greater than equal to
+print(3 <= 2)   # less than equal to
+print(3 == 3)   # equal to
+print(3 != 2)   # not equal to
