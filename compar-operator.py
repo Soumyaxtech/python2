33bd55fbffb7be3ca253a381c2aea5985601367c
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-01-09 10:19:30 +0530

    logical operations

--- compar-operator.py
+++ compar-operator.py
@@ -4,4 +4,4 @@ print(3 < 2)    # less than
 print(3 >= 2)   # greater than equal to
 print(3 <= 2)   # less than equal to
 print(3 == 3)   # equal to
-  # not equal to
+print(3 != 2)   # not equal to
---
+++ logic-operator.py
@@ -0,0 +1,4 @@
+#   DIFF LOGICAL OPERATOR (AND OR NOT)
+print(3 > 2 and 4 > 2)   # both condition should be true then it will give result true
+print(2 < 1 or 1 < 2)    # if any one condition is true then return true
+print(not 3 < 2)     # if condition false return true and if condition true return false
