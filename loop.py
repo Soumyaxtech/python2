3b648e569fbaa3d8699bd5d7a716b161a0e20577
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-01-11 09:23:58 +0530

    loop operations

---
+++ loop.py
@@ -0,0 +1,12 @@
+# num = int(input("enter number : "))
+# i = 1
+# while i <= num :  #  WHILE LOOP   ######################
+#     print(i * "%") # FOR PATTERN PRINTING **********
+#     i = i + 1
+
+
+    # FOR LOOP ******************
+num = int(input("enter number : "))
+i = 1
+for i in range(num):
+    print(i+1)
--- while-loop.py
+++
@@ -1,5 +0,0 @@
-num = int(input("enter number : "))
-i = 1
-while i <= num :
-    print(i * "%") # FOR PATTERN PRINTING **********
-    i = i + 1
