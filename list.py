44c866950030f47e6296dbfbad5e00e0f8a1cdab
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-01-17 11:53:01 +0530

    append and insert in list

--- list.py
+++ list.py
@@ -1,5 +1,11 @@
 # LIST IS COMPLEX DATA TYPE , DIFF TYPES OF DATATYPE IS STORED IN IT
 # INDEX LEFT->RIGHT (0 ,1,..........) RIGHT->LEFT(-1,-2......)
-marks = [95,98,92,"MATH"]
+marks = [95,98,92,81]
 print(marks[-1]) # ONLY PRINT -1 INDEX
-print(marks[0:2]); # it only print upto index 2 (0,1)will be print not index 2
+print(marks[0:2]);   # it only print upto index 2 (0,1)will be print not index 2
+for score in marks:   # PRINTING LIST USING LOOPS
+    print(score)
+marks.append(99)  # TO INSERT ANYTHING AT THE END OF LIST
+print(marks)
+marks.insert(0,85) # TO INSERT ANYWHERE IN LIST FIRST WE GIVE INDEX THEN MARKS
+print(marks)
