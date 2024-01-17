d643d3d79abb31da000fcb0d6158d07bae96461c
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-01-17 11:35:03 +0530

    list printing

---
+++ list.py
@@ -0,0 +1,5 @@
+# LIST IS COMPLEX DATA TYPE , DIFF TYPES OF DATATYPE IS STORED IN IT
+# INDEX LEFT->RIGHT (0 ,1,..........) RIGHT->LEFT(-1,-2......)
+marks = [95,98,92,"MATH"]
+print(marks[-1]) # ONLY PRINT -1 INDEX
+print(marks[0:2]); # it only print upto index 2 (0,1)will be print not index 2
