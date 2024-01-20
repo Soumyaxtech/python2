5665b09b4153da575bacad1c573236e4324dac3d
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-01-21 00:03:56 +0530

    counting expances using for loop

--- for-loop.py
+++ for-loop.py
@@ -1,6 +1,14 @@
 # calculating total expances through-out the year
+
+
 exp = [250,980,673,123,789,556,800,230,456,890,900,1600]
 total = 0
-for item in exp:
-    total=total+item
-print (total)
+for i in range(len(exp)):
+    print("month ",i+1, ": expances ",exp[i])
+    total=total+exp[i]
+print ("total expances through-out year ",total)
+
+# printing 1-10 using for loop
+
+# for i in range(0,10):
+#     print (i+1)   # i is a variable here
