6b603a96d87d95d846466adbce0b1e8aaed4cf77
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-01-25 00:10:42 +0530

    dictionary in python (clear operation)

--- dictionary.py
+++ dictionary.py
@@ -1,8 +1,11 @@
-d = {"tom ":123,"joe":567,"max":900}
-print(d["tom "])
+d = {"tom":123,"joe":567,"max":900}
+print(d["tom"])
 d["jenny"]= 345 # ADD ITEM IN DICTIONARY
 print(d)
 del d["max"]  # IT DELETE ITEM FROM DICTIONARY
 print(d)
 for key in d:
     print(key,":",d[key])
+print("tom" in d)   # TO CHECK IF THIS ITEM IS IN DICTIONARY OR NOT
+d.clear()  # TO EMPTY THE DICTIONARY
+print(d)
