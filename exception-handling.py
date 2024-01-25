f445fca63ee75492406e4c87403313725750f2b8
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-01-26 00:18:14 +0530

    diff error handling like Type-error and ZeroDivisionError

--- exception-handling.py
+++ exception-handling.py
@@ -1,8 +1,13 @@
-x = int(input("enter 1st number "))
-y = int(input("enter 2nd number "))
+x = (input("enter 1st number "))
+y = (input("enter 2nd number "))
 try:
-    z = x/y              #********THIS IS CALLED EXCEPTION HANDLING WE HAVE TO WRITE
-except Exception as e:    # (try:then the code where exception might occur then except ---------)
+    z = int(x)/int(y)             #********THIS IS CALLED EXCEPTION HANDLING WE HAVE TO WRITE
+except ZeroDivisionError as e:    # (try:then the code where exception might occur then except ---------)
     print("exception is", e)
     z = None
 print("the ans is", z)
+# except TypeError as e:
+#     print("typeerror occur")  #except Exception as e
+                               # print("error is ",type(e))
+#     z=None
+# print("the ans is", z)
