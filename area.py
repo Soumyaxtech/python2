5ac06fa32982f166c1f311517b2fe0904e1e599b
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-01-25 23:32:30 +0530

    calculate area of triangle

---
+++ area.py
@@ -0,0 +1,8 @@
+def calculateArea(base,height):
+    print("__name__ :",__name__)
+    area=(1/2)*(base*height)
+    return area
+if __name__=="__main__":
+    area1=calculateArea(10,20)
+    print("i am in area.py")
+    print("area is",area1)
