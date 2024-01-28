69b555c2dcd2c2a00a6edbbe9fe6b583e2e5d526
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-01-28 20:47:22 +0530

    mean and median in python

--- mean-median.py
+++ mean-median.py
@@ -1,6 +1,15 @@
 def mean(numbers):
     total=sum(numbers)
     return total/len(numbers)
+def median(numbers):
+    sortednum=sorted(numbers)
+    if len(sortednum)%2==0:
+        median1=sortednum[len(sortednum)//2-1]+sortednum[len(sortednum)//2]/2
+    else:
+        median1=sortednum[len(sortednum)//2]
+    return median1
 numbers=[4,5,6]
 mean=mean(numbers)
 print(mean)
+median=median(numbers)
+print(median)
